5a0b5e57e29ede48af8f88d8de71382d007ccdfb
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-17 09:33:31 -0700

    update controller for index and show

---
+++ BootMVCLibrary/src/main/java/com/skilldistillery/bootmvc/controllers/BookController.java
@@ -0,0 +1,35 @@
+package com.skilldistillery.bootmvc.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.skilldistillery.JPACrudProject.entities.Book;
+import com.skilldistillery.bootmvc.data.BookDAO;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	BookDAO dao;
+	
+	@RequestMapping(path="/")
+	public String index() {
+	  return "WEB-INF/index.jsp";
+	  // return "index"; // if using a ViewResolver.
+	}
+	
+	@RequestMapping(path = "getBook.do")
+	public String showBook(@RequestParam Integer bid, Model model) {
+		Book book = dao.findById(bid);
+		
+		model.addAttribute("book", book);
+		
+		
+		
+		return "WEB-INF/book/show.jsp";
+	}
+
+}
