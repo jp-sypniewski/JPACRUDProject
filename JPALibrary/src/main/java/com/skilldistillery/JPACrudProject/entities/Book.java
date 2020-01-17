1e6588ba0a46bc19c04f8a396dd9ac5d08a41892
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-16 22:39:35 -0700

    update gradle for junit dependencies

--- JPALibrary/src/main/java/com/skilldistillery/JPACrudProject/entities/Book.java
+++ JPALibrary/src/main/java/com/skilldistillery/JPACrudProject/entities/Book.java
@@ -1,10 +1,64 @@
 package com.skilldistillery.JPACrudProject.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="books")
 public class Book {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	
+	private String name;
+	
+	private Integer completed;
+	
+	@Column(name="checkin_status")
+	private String checkinStatus;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getCompleted() {
+		return completed;
+	}
+
+	public void setCompleted(Integer completed) {
+		this.completed = completed;
+	}
+
+	public String getCheckinStatus() {
+		return checkinStatus;
+	}
+
+	public void setCheckinStatus(String checkinStatus) {
+		this.checkinStatus = checkinStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", " + (name != null ? "name=" + name + ", " : "")
+				+ (completed != null ? "completed=" + completed + ", " : "")
+				+ (checkinStatus != null ? "checkinStatus=" + checkinStatus : "") + "]";
+	}
+	
+	
 }
