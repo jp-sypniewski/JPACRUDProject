22833f7e3dc0b59d7e87cf04ef6f506155320c01
jp-sypniewski <jpsypniewski@icloud.com>
2020-01-20 08:41:59 -0700

    update application file

--- BootMVCLibrary/src/main/java/com/skilldistillery/bootmvc/BootMvcLibraryApplication.java
+++ BootMVCLibrary/src/main/java/com/skilldistillery/bootmvc/BootMvcLibraryApplication.java
@@ -3,10 +3,17 @@ package com.skilldistillery.bootmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.JPACrudProject")
-public class BootMvcLibraryApplication {
+public class BootMvcLibraryApplication extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(BootMvcLibraryApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootMvcLibraryApplication.class, args);
